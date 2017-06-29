84a4614698ede9e0b221e489351535338d211370
Raghavendra Gautam <rgautam98@users.noreply.github.com>
2017-06-29 02:57:58 -0700

    Update day1.sql

--- day1.sql
+++ day1.sql
@@ -5,3 +5,22 @@
 insert into student (id, name) values (9, 'Gautam'); -- Inserting in to the table
 desc student; -- Describing the table student
 select * from student; -- Typical select query
+
+
+-- Multiple insert from the GUI
+--INSERT ALL
+--insert into student  values (&id, &name) ;
+
+--SELECT * FROM dual;
+-- Inserting in to the table
+--desc student; -- Describing the table student
+--select * from student; -- Typical select query
+
+
+drop table student;
+create table student (id number, name varchar(20));
+
+INSERT ALL
+into student (id, name) values (100, 'Foo')
+into student (id, name) values (300, 'Car')
+SELECT * FROM dual;
