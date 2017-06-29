fa487c739a9ee83a869985a8deec59b4a7bd8fe3
Raghavendra Gautam <rgautam98@users.noreply.github.com>
2017-06-29 02:32:32 -0700

    Create day1.sql

---
+++ day1.sql
@@ -0,0 +1,5 @@
+-- This is what happened on day 1, dated: 29.06.2017 at 2:00 PM to 4:00 PM
+
+create table student (id number, name varchar(20));
+insert into student (id, name) values (9, 'Gautam');
+select * from student;
