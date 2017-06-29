0fe602728b8bf4c8ca12fa85283f7f33c6b42a60
Raghavendra Gautam <rgautam98@users.noreply.github.com>
2017-06-29 02:36:39 -0700

    Update day1.sql

--- day1.sql
+++ day1.sql
@@ -1,5 +1,7 @@
 -- This is what happened on day 1, dated: 29.06.2017 at 2:00 PM to 4:00 PM
 
-create table student (id number, name varchar(20));
-insert into student (id, name) values (9, 'Gautam');
-select * from student;
+--create table student (id number, name varchar(20)); -- Creating tables you see.
+
+insert into student (id, name) values (9, 'Gautam'); -- Inserting in to the table
+desc student; -- Describing the table student
+select * from student; -- Typical select query
