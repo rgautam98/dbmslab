15dd56cbf2e5200153a24dbad3341db104e90a36
Raghavendra Gautam <rgautam98@users.noreply.github.com>
2017-07-06 02:56:57 -0700

    Update day1.sql

--- day1.sql
+++ day1.sql
@@ -1,5 +1,3 @@
--- This is what happened on day 1, dated: 29.06.2017 at 2:00 PM to 4:00 PM
-
 --create table student (id number, name varchar(20)); -- Creating tables you see.
 
 insert into student (id, name) values (9, 'Gautam'); -- Inserting in to the table
