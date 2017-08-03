f890023afa90fceb6b1a872d4d9141952c071b3e
Raghavendra Gautam <rgautam98@users.noreply.github.com>
2017-08-03 03:13:42 -0700

    Update misc.sql

--- misc.sql
+++ misc.sql
@@ -1,11 +1,15 @@
---CREATE TABLE sailors ( 
---rg integer not null,
---sname varchar(32),
---CONSTRAINT PK_sailors PRIMARY KEY (rg) 
---);
+--drop table sailors;
+
+CREATE TABLE sailors ( 
+rg int not null,
+sname varchar(32),
+CONSTRAINT PK_sailors PRIMARY KEY (rg) 
+);
 
 CREATE TABLE reserves ( 
-rgid integer not null FOREIGN KEY (rgid) REFERENCES sailors(rg),
+rgid int,
 bid integer not null,
-sailing date not null
+sailing date not null,
+FOREIGN KEY (rgid) REFERENCES sailors(rg)
+
 );
