49e65ca6c18ca2c16156a47d990affde5a2e90ff
Raghavendra Gautam <rgautam98@users.noreply.github.com>
2017-09-14 02:56:57 -0700

    Added the sum of 2 numbers

--- plsql/sum-2-nos.sql
+++ plsql/sum-2-nos.sql
@@ -1,1 +1,15 @@
-HEllp World
+DECLARE
+  a number;
+  b number;
+  c number;
+BEGIN
+  -- Taking input from the user.
+  a := &a; b := &b; c := &c;
+  if( (a>b) and (a>c) ) then dbms_output.put_line(a);
+  else
+    if( (b>a) and (b>c) ) then dbms_output.put_line(b);
+    else
+      dbms_output.put_line(c);
+    end if;
+  end if;
+END;
